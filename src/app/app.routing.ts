ffdba546800021ef06b36d4359fa5d2554aa1539
rm <biffbangpow767@yahoo.com>
2017-10-26 16:52:08 -0700

    display individual albums

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { MarketplaceComponent }   from './marketplace/marketplace.component';
+import { AlbumDetailComponent }   from './album-detail/album-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -16,7 +17,11 @@ const appRoutes: Routes = [
   {
     path: 'marketplace',
     component: MarketplaceComponent
-  }
+  },
+  {
+   path: 'albums/:id',
+   component: AlbumDetailComponent
+ }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
